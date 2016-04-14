93041412340211ba50092934bdad8f657f0e7f23
Connor Brinton <connor.brinton@gmail.com>
2016-04-13 21:06:29 -0700

    Add inversion for Amazon ebooks.

--- js/ytnorel.js
+++ js/ytnorel.js
@@ -1,7 +1,6 @@
-movie_player.addEventListener("onStateChange", "onytplayerStateChange");
-
-function onytplayerStateChange(newState) {
+movie_player.addEventListener("onStateChange", function onytplayerStateChange(newState) {
+  console.log("New State: " + newState);
   if(newState == 0) {
     movie_player.seekTo(0, true);
   }
-}
+});
